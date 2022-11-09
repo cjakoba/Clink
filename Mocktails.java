1c380daac3b4cc134006fc6c0c67a00cc69d6d15
Anshuadhikari <anshu.adhikari10@gmail.com>
2022-11-08 19:52:38 -0500

    Added name

---
+++ Mocktails.java
@@ -0,0 +1,18 @@
+public class Mocktails {
+    private String name;
+    private String descripton;
+    private double price;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return descripton;
+    }
+
+    public Double getPrice(){
+        return price;
+    }
+
+}
---
+++ Retailer.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Retailer
+{
+private String menu;
+
+ArrayList<Mocktails> mocktails;
+
+public static void addDrinks(){
+//add products to the menu
+}
+
+public static void updateMenu(){
+    //remove items
+    //change name of item
+    //change description of item
+    //change price of item
+}
+
+public static void calculateProfits()
+{
+    //add total amount of money from each sale
+}
+
+
+
+
+}
