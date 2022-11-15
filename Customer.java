b278b4f0615b904167f121804e5d276041ad9418
Cortega2-1 <cortega2@mail.umw.edu>
2022-11-15 16:16:19 -0500

    added some methods to customer class

--- Customer.java
+++ Customer.java
@@ -2,5 +2,35 @@ import java.util.ArrayList;
 
 public class Customer
 {
-    private ArrayList<Mocktails> carts;
+    private String name;
+    private int contactNumber;
+    private String address;
+
+    public Customer(String name, int contactNumber, String address)
+    {
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.address = address;
+    }
+    public String myInfo()
+    {
+        return name + " " + contactNumber + " " + address;
+    }
+
+    //just an example of how receipt will look, I know we need to print it on gui later
+    public void printReceipt()
+    {
+
+          System.out.println(
+                    "--------------------------------"
+                            + "-----------FEE RECEIPT----"
+                            + "--------------------------"
+                            + "--------------------------"
+                            + "-------------------\n");
+
+            System.out.println("Name: " + name);
+            System.out.println("Contact Number: " + contactNumber);
+            System.out.println("Address: " + address);
+
+    }
 }
